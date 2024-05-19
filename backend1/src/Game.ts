5706337eb303b8824b4fa1c9307814a9b594378b
Dhaval Gupta <dhavalgupta302@gmail.com>
2024-05-19 19:34:55 +0530

    Completing the backend1

--- backend1/src/Game.ts
+++ backend1/src/Game.ts
@@ -1,5 +1,6 @@
 import { Chess } from "chess.js";
 import { WebSocket } from "ws";
+import { GAME_OVER, INIT_GAME, MOVE } from "./messages";
 
 export class Game {
 
@@ -7,6 +8,7 @@ export class Game {
     public player2: WebSocket;
     public board: Chess;
     private startTime: Date;
+    private moveCount = 0;
 
 
     constructor(player1: WebSocket, player2: WebSocket) {
@@ -14,20 +16,80 @@ export class Game {
         this.player2 = player2;
         this.board = new Chess();
         this.startTime = new Date();
+
+        //let the players know the game has started
+        this.player1.send(JSON.stringify({
+            type: INIT_GAME,
+            payload: {
+                color: "white"
+            }
+        }))
+        
+        this.player2.send(JSON.stringify({
+            type: INIT_GAME,
+            payload: {
+                color: "black"
+            }
+        }))
     }
 
-    public makeMove(socket: WebSocket, move: string) {
-        //validation
-        // it is this users move
-        //is the move valid
+    public makeMove(socket: WebSocket, move: {
+        from: string;
+        to: string;
+    }) {
+        //validation the type of move using zod
+        if(this.moveCount % 2 === 0 && socket !== this.player1) {
+            console.log("early return 1")
+            return;
+        } 
+        if(this.moveCount % 2 === 1 && socket !== this.player2) {
+            console.log("early return 2")
+            return;
+        }
+        
+        console.log("did not early return");
 
+        try {
+            this.board.move(move);
+        } catch(e) {
+            console.log(e);
+            return;
+        }
 
-
-        //update the board
-        //push the move
+        console.log("move succeded");
 
         //check if game is over
+        if(this.board.isGameOver()) {
+            this.player1.emit(JSON.stringify({
+                type: GAME_OVER,
+                payload: {
+                    winner : this.board.turn() === "w" ? "black" : "white"
+                }
+            }))
 
-        //send the updated board to both users 
+            this.player2.emit(JSON.stringify({
+                type: GAME_OVER,
+                payload: {
+                    winner : this.board.turn() === "w" ? "black" : "white"
+                }
+            }))
+            return;
+        }
+
+        console.log(this.board.moves().length % 2 === 0)
+        if(this.moveCount % 2 === 0) {
+            console.log("sent 1")
+            this.player2.send(JSON.stringify({
+                type: MOVE,
+                payload: move
+            }))
+        } else {
+            console.log("sent 2")
+             this.player1.send(JSON.stringify({
+                type: MOVE,
+                payload: move
+             }))
+        }
+        this.moveCount++;
     }
 }
