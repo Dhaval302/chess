643b96ce18edbfb517e2d46f9e2210dbf9e2e395
Dhaval Gupta <dhavalgupta302@gmail.com>
2024-05-20 23:55:26 +0530

    Adding the landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Landing } from './screens/Landing';
+import { Game } from './screens/Game';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <button className='bg-red-200'>
-        Join room 
-      </button>
+    <div className='h-screen bg-slate-900'>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} /> 
+        <Route path="/game" element={<Game  />} /> 
+      </Routes>
+    </BrowserRouter>
+    </div>
     </>
   )
 }
